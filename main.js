cf94ab51aaed15e275947f40fa8404db16994d48
Borsos Imre <borsos.imre@BOLYAI.LOCAL>
2024-10-11 14:46:13 +0200

    igen igen megvan igen

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ const th_firstname = document.createElement('th');
 tr.appendChild(th_firstname);
 th_firstname.innerHTML='keresztnév';
  
-th_lastname.colSpan=2;
+th_firstname.colSpan=2;
  
 const tbody = document.createElement('tbody');
 table.appendChild(tbody);
@@ -58,72 +58,94 @@ th_married.innerHTML="Házas";
 const th_pet=document.createElement('th');
 tr.appendChild(th_pet);
 th_pet.innerHTML="Háziálata";
- 
-for(const pers of array){
-    const tbody_tr = document.createElement('tr');
-    tbody.appendChild(tbody_tr);
- 
-    const tbody_td_lastname = document.createElement('td');
-    tbody_tr.appendChild(tbody_td_lastname);
-   
-    tbody_td_lastname.innerHTML = pers.lastname;
- 
-    const tbody_td_firstname = document.createElement('td');
-    tbody_tr.appendChild(tbody_td_firstname);
-   
-    tbody_td_firstname.innerHTML = pers.firstname1;
-
-
- 
-    if(pers.firstname2 === undefined){
-        tbody_td_firstname.colSpan = 2
-    }
-    else{
-        const tbody_td_firstname = document.createElement('td');
-        tbody_tr.appendChild(tbody_td_firstname);
-       
-        tbody_td_firstname.innerHTML = pers.firstname2;
-    }
-    tbody_tr.addEventListener('click', function(e){
-        //console.log('clicked');
-        const selected = tbody.querySelector('.selected');
-        e.currentTarget.classList.add('selected');
-        
-
-        if (selected != undefined){
-            selected.classList.remove('selected');
-        }
-          
-    })
-
-    const td_married= document.createElement('td');
-    tbody_tr.appendChild(td_married);
-    td_married.innerHTML = pers.married;
-
-    const td_pet= document.createElement('td');
-    tbody_tr.appendChild(td_pet);
-    td_pet.innerHTML = pers.pet;
-
-    if(pers.married===true){
-        td_married.innerHTML="Igen"
-    }
-    else{
-        td_married.innerHTML="Nem"
-    }
- 
-}
 
 const form = document.getElementById("form");
 form.addEventListener("submit", function(e){
-    const lastname =getElementById("lastname");
-    const firstname1 =getElementById("firstname1");
-    const firstname2 =getElementById("firstname2");
-    const married =getElementById("married");
-    const pet =getElementById("pet");
+    e.preventDefault();
+    const lastname =document.getElementById("lastname");
+    const firstname1 =document.getElementById("firstname1");
+    const firstname2 =document.getElementById("firstname2");
+    const married =document.getElementById("married");
+    const pet =document.getElementById("pet");
 
     const lastnamevalue=lastname.value;
-    const firrstname1value=firstname1.value;
-    const firstname2value=firstname2.value;
+    const firstname1value=firstname1.value;
+    let firstname2value=firstname2.value;
     const marriedvalue=married.checked;
     const petvalue=pet.value;
+
+    if (firstname2value===""){
+        firstname2value=undefined;
+    }
+    const newperson={
+        firstname1: firstname1value,
+        firstname2: firstname2value,
+        lastname: lastnamevalue,
+        married: marriedvalue,
+        pet: petvalue
+    }
+
+    array.push(newperson);
+    console.log(array);
+    rendertable();
+    
 })
+rendertable();
+function rendertable(){
+    tbody.innerHTML="";
+    for(const pers of array){
+        const tbody_tr = document.createElement('tr');
+        tbody.appendChild(tbody_tr);
+     
+        const tbody_td_lastname = document.createElement('td');
+        tbody_tr.appendChild(tbody_td_lastname);
+       
+        tbody_td_lastname.innerHTML = pers.lastname;
+     
+        const tbody_td_firstname = document.createElement('td');
+        tbody_tr.appendChild(tbody_td_firstname);
+       
+        tbody_td_firstname.innerHTML = pers.firstname1;
+    
+  
+
+        if(pers.firstname2 === undefined){
+            tbody_td_firstname.colSpan = 2;
+        }
+        else{
+            const tbody_td_firstname = document.createElement('td');
+            tbody_tr.appendChild(tbody_td_firstname);
+           
+            tbody_td_firstname.innerHTML = pers.firstname2;
+        }
+        tbody_tr.addEventListener('click', function(e){
+            //console.log('clicked');
+            const selected = tbody.querySelector('.selected');
+            e.currentTarget.classList.add('selected');
+            
+    
+            if (selected != undefined){
+                selected.classList.remove('selected');
+            }
+              
+        })
+    
+        const td_married= document.createElement('td');
+        tbody_tr.appendChild(td_married);
+        td_married.innerHTML = pers.married;
+    
+        const td_pet= document.createElement('td');
+        tbody_tr.appendChild(td_pet);
+        td_pet.innerHTML = pers.pet;
+    
+        if(pers.married===true){
+            td_married.innerHTML="Igen"
+        }
+        else{
+            td_married.innerHTML="Nem"
+        }
+     
+
+     
+    }
+}
